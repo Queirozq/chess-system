7431f60fc5f17fe1f96df442ab39f6ccb92875f7
Matheus Queiroz <scririft@gmail.com>
2021-05-20 02:02:20 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+}
+
+
