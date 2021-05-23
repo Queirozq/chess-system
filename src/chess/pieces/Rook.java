7a6f0866949769c88243fadaa640d316f0e48149
Matheus Queiroz <scririft@gmail.com>
2021-05-23 03:28:29 -0300

    Possible moves of a piece²

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,4 +15,8 @@ public class Rook extends ChessPiece {
     public String toString(){
         return "R";
     }
+    @Override
+    public boolean[][] possibleMoves() {
+        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+    }
 }
